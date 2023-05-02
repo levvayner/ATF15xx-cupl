83c51a31e6ac77c1a157aed47e7ad474cec99a87
Lev Vayner <lev.vayner@vaynersystems.com>
2023-05-02 14:17:49 -0400

    pins view ...

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { registerDeployJedCommand } from "./svc.deploy-jed";
-import {
-    registerCloneProjectCommand,
-    registerCloseProjectCommand,
-    registerConfigureProjectCommand,
-    registerCreateProjectCommand,
-    registerDeleteFileCommand,
-    registerImportProjectCommand,
-    registerOpenProjectCommand,
+import { 
+    registerCloneProjectCommand, registerCloseProjectCommand, registerConfigureProjectCommand, 
+    registerCreateProjectCommand, registerDeleteFileCommand, registerImportProjectCommand, registerOpenProjectCommand
 } from "./svc.project";
 import { registerCompileProjectCommand } from "./svc.build";
 import { registerISPCommand } from "./svc.atmisp";
@@ -23,45 +16,51 @@ import {
     ProjectTasksProvider,
     projectTasksProvider,
 } from "./explorer/project-tasks-provider";
-import {
-    checkPrerequisiteCommand,
-    cloneProjectCommand,
-    closeProjectCommand,
-    compileProjectCommand,
-    configureProjectCommand,
-    createProjectCommand,
-    deleteEntryCommand,
-    deployJedCommand,
-    deploySvfCommand,
-    editEntryCommand,
-    importProjectCommand,
-    openInExplorerCommand,
-    openProjectCommand,
-    openSettingsCommand,
-    runISPCommand,
-    runMiniProCommand,
-} from "./vs.commands";
+import * as cmd from "./vs.commands";
 import { registerOpenInExplorerCommand } from "./explorer/fileFunctions";
 import { registerVariableExtensionProvider } from "./editor/variableProvider";
 import { registerDeploySvfCommand } from "./svc.deploy-svf";
-import { Project } from "./types";
 import { StateProjects, stateProjects } from "./state.projects";
 import { registerOpenSettingsCommand, registerEditFileCommand } from "./extension/file-provider";
 import { registerSemanticTokenProvider } from "./inspect/sematic-token-provider";
-import { ChipViewProvider, registerChipViewPanelProvider } from "./editor/chip-view";
-import { PinViewProvider, registerPinViewPanelProvider } from "./editor/pin-view";
+import { registerChipViewPanelProvider } from "./editor/chip-view";
+import { registerPinViewPanelProvider } from "./editor/pin-view";
 
-// import { PldEditorProvider } from './editor/pldEditorProvider.ts.old';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log("Activating VS VS Programmer extension");
 
+    await registerProjectViewProviders(context);
+    await registerCommands(context);    
+    await registerCheckPrerequisite(cmd.checkPrerequisiteCommand, context);
+    await registerCodeProvider(context);    
+}
+export function deactivate() {}
+
+async function registerCommands(context: vscode.ExtensionContext){
+    await registerOpenInExplorerCommand(cmd.openInExplorerCommand, context);
+    await registerOpenSettingsCommand(cmd.openSettingsCommand, context);
+    await registerEditFileCommand(cmd.editEntryCommand, context);
+    await registerDeploySvfCommand(cmd.deploySvfCommand, context);
+    await registerCreateProjectCommand(cmd.createProjectCommand, context);
+    await registerCloneProjectCommand(cmd.cloneProjectCommand, context);
+    await registerConfigureProjectCommand(cmd.configureProjectCommand, context);
+    await registerOpenProjectCommand(cmd.openProjectCommand, context);
+    await registerImportProjectCommand(cmd.importProjectCommand, context);
+    await registerCloseProjectCommand(cmd.closeProjectCommand, context);
+    await registerCompileProjectCommand(cmd.compileProjectCommand, context);
+    await registerDeleteFileCommand(cmd.deleteEntryCommand, context);
+    await registerDeployJedCommand(cmd.deployJedCommand, context);
+    await registerISPCommand(cmd.runISPCommand, context);
+    await registerMiniProCommand(cmd.runMiniProCommand, context);
+}
+
+async function registerProjectViewProviders(context: vscode.ExtensionContext){
+    await ProjectTasksProvider.init();
     const projectFileProvider = await ProjectFilesProvider.instance();
+
     await StateProjects.init();
+
     const rootPath =
         vscode.workspace.workspaceFolders &&
         vscode.workspace.workspaceFolders.length > 0
@@ -71,48 +70,15 @@ export async function activate(context: vscode.ExtensionContext) {
         projectFileProvider.setWorkspace(rootPath[0]);
         vscode.window.setStatusBarMessage("No open folder found!", 2000);
     }
-    //path of executing extension
-    context.extensionPath;
+    vscode.window.registerTreeDataProvider("vs-cupl-project-files", projectFileProvider);
+    vscode.window.registerTreeDataProvider("vs-cupl-project-tasks", projectTasksProvider);
 
-    await ProjectTasksProvider.init();
-
-    vscode.window.registerTreeDataProvider(
-        "vs-cupl-project-files",
-        projectFileProvider
-    );
-    vscode.window.registerTreeDataProvider(
-        "vs-cupl-project-tasks",
-        projectTasksProvider
-    );
-    await registerOpenInExplorerCommand(openInExplorerCommand, context);
-    await registerOpenSettingsCommand(openSettingsCommand, context);
-    await registerEditFileCommand(editEntryCommand, context);
-    await registerDeploySvfCommand(deploySvfCommand, context);
-    await registerCreateProjectCommand(createProjectCommand, context);
-    await registerCloneProjectCommand(cloneProjectCommand, context);
-    await registerConfigureProjectCommand(configureProjectCommand, context);
-    await registerOpenProjectCommand(openProjectCommand, context);
-    await registerImportProjectCommand(importProjectCommand, context);
-    await registerCloseProjectCommand(closeProjectCommand, context);
-    await registerCompileProjectCommand(compileProjectCommand, context);
-    await registerDeleteFileCommand(deleteEntryCommand, context);
-    await registerDeployJedCommand(deployJedCommand, context);
-    await registerISPCommand(runISPCommand, context);
-    await registerMiniProCommand(runMiniProCommand, context);
-
-    await registerCheckPrerequisite(checkPrerequisiteCommand, context);
-
-    await registerVariableExtensionProvider(context);
-
-    
     await registerChipViewPanelProvider(context);
     await registerPinViewPanelProvider(context);
+}
 
+async function registerCodeProvider(context: vscode.ExtensionContext) {
+    await registerVariableExtensionProvider(context);
     await registerSemanticTokenProvider(context);
 }
-// This method is called when your extension is deactivated
-export function deactivate() {}
-
-
-
 
